c62c30fca8a980beaf99ed65918c36355dce8f59
Yueisad <jackleekpop35@gmail.com>
2023-05-10 16:36:33 -0700

    Saving Models and displaying the data

--- Regression.py
+++ Regression.py
@@ -3,6 +3,9 @@ import numpy as np
 import sklearn
 from sklearn import linear_model
 from sklearn.utils import shuffle
+import matplotlib.pyplot as pyplot
+import pickle
+from matplotlib import style
 
 data = pd.read_csv("student-mat.csv", sep = ";")
 
@@ -12,16 +15,33 @@ predict = "G3"
 
 X = np.array(data.drop([predict], axis = 1))
 y = np.array(data[predict])
-
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size = 0.1)
 
-linear = linear_model.LinearRegression()
+'''
+#Training the model
+best = 0
+for _ in range(30):
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size = 0.1)
 
-linear.fit(x_train, y_train)
+    linear = linear_model.LinearRegression()
 
-#will show us what the score of the best fit is for the test data
-acc = linear.score(x_test, y_test)
-print(acc)
+    linear.fit(x_train, y_train)
+
+    #will show us what the score of the best fit is for the test data
+    acc = linear.score(x_test, y_test)
+    print(acc)
+
+    if acc > best:
+        best = acc
+ #saves a pickle file in the directory aka the model
+        with open("student.pickle", "wb") as f :
+            pickle.dump(linear, f)
+'''
+
+
+pickle_in = open("student.pickle", "rb")
+#load model in the variable linear
+linear = pickle.load(pickle_in)
 
 print('Coefficient: \n' , linear.coef_)
 print('Intercept: \n' , linear.intercept_)
@@ -32,4 +52,10 @@ predictions = linear.predict(x_test)
 for x in range (len(predictions)) :
     print(predictions[x], x_test[x], y_test[x])
 
-#adding this comment to test the branch push
+#change the p value to other headers to see the correlation
+p = 'absences'
+style.use("ggplot")
+pyplot.scatter(data[p], data["G3"])
+pyplot.xlabel(p)
+pyplot.ylabel("Final Grade")
+pyplot.show()
